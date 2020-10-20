631d6b7ab6be72b82883d1845ffde307391bf6ff
Luan <luanreisciribelli@gmail.com>
2020-10-20 15:30:16 -0300

    classes utils

--- src/Utils/ArestaDuasPontas.h
+++ src/Utils/ArestaDuasPontas.h
@@ -1,1 +1,24 @@
- 
+
+#ifndef ARESTADUASPONTAS_H
+#define ARESTADUASPONTAS_H
+
+
+
+using namespace std;
+
+/**
+*   Universidade Federal de Juiz de Fora
+*   Criador: Luan Reis Ciribelli e Renan Nunes
+*   vers�o: 1.0
+*/
+class ArestaDuasPontas
+{
+    public:
+
+
+
+    private:
+
+};
+
+#endif // ARESTADUASPONTAS_H
--- src/Utils/ListaArestasDuasPontas.h
+++ src/Utils/ListaArestasDuasPontas.h
@@ -1,1 +1,25 @@
- 
+
+
+#ifndef LISTAARESTADUASPONTAS_H
+#define LISRAARESTADUASPONTAS_H
+
+
+
+using namespace std;
+
+/**
+*   Universidade Federal de Juiz de Fora
+*   Criador: Luan Reis Ciribelli e Renan Nunes
+*   vers�o: 1.0
+*/
+class ListaArestaDuasPontas
+{
+    public:
+
+
+
+    private:
+
+};
+
+#endif //LISTAARESTADUASPONTAS_H
